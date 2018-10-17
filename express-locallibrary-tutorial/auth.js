838022b4b4ea23484499432f87dbe42ba9afd24a
Chris Figgins <figginsc@gmail.com>
2018-10-17 12:39:33 -0400

    Noticed a typo on the auth part - fixed

--- express-locallibrary-tutorial/auth.js
+++ express-locallibrary-tutorial/auth.js
@@ -7,8 +7,8 @@ module.exports = (passport) => {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
-            clientID: process.env.key_one.googleClientID,
-            clientSecret: process.env.key_one.googleClientSecret,
+            clientID: process.env.googleClientID,
+            clientSecret: process.env.googleClientSecret,
             callbackURL: 'https://figgins-playground.herokuapp.com/'
         },
         (token, refreshToken, profile, done) => {
